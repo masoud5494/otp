91277c2a2d8859e8e2151c901ad68a137f4405e4
Masoud Karimi <karimi.masoud5494@gmail.com>
2020-11-16 14:44:35 +0330

    added otp module

---
+++ otp/src/main/java/mkn/libs/otp/OtpEditText.kt
@@ -0,0 +1,8 @@
+package mkn.libs.otp
+
+import android.content.Context
+import androidx.appcompat.widget.AppCompatEditText
+
+class OtpEditText(context: Context): AppCompatEditText(context) {
+    
+}
